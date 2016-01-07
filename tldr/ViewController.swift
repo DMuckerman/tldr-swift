e05710e49f592274610419bbe90ef86f789e9076
Daniel Muckerman <danielmuckerman@me.com>
2016-01-06 22:27:37 -0500

    Added TableView for platforms, and made it dynamically update with the platforms for each command.

--- tldr/ViewController.swift
+++ tldr/ViewController.swift
@@ -25,11 +25,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var commands: [String: [JSON]] = [:]
+    var items: [String] = []
+    let textCellIdentifier = "TextCell"
 
     @IBOutlet weak var textInput: UITextField!
     @IBOutlet weak var markdownPage: UIWebView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,8 +58,36 @@ class ViewController: UIViewController {
         } else {
             print("Invalid filename/path.")
         }
-
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "TextCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = items[row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(items[row])
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -103,13 +134,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didSearch(sender: UITextField) {
-        print(sender.text!)
+        //print(sender.text!)
         markdownPage.scrollView.contentOffset = CGPoint(x: 0, y: 0)
         var markdown = Markdown()
         var result = commands[sender.text!]
         print(result)
         if (result != nil) {
-            print(result![0])
+            //print(result!)
+            items.removeAll()
+            for item in result! {
+                items.append(item.string!)
+            }
+            print(items)
+            self.tableView.reloadData()
             let path = "pages/\(result![0])/\(sender.text!)"
             if let path = NSBundle.mainBundle().pathForResource(path, ofType: "md") {
                 do {
@@ -124,6 +161,8 @@ class ViewController: UIViewController {
             }
         } else {
             markdownPage.loadHTMLString("", baseURL: nil)
+            items.removeAll()
+            self.tableView.reloadData()
         }
     }
     
